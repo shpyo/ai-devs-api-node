b0228aac50b996c3acc8d454987303801bce63ae
shpyo <piotr@fronthero.pl>
2024-04-13 18:05:04 +0200

    feat: add polish prompt to gnome task

--- 16-gnome/index.ts
+++ 16-gnome/index.ts
@@ -5,11 +5,15 @@ interface GnomeTaskResponse {
   url: string;
 }
 
+const prompt = `Na rysunku będzie przedstawiony skrzat.
+Zadanie polega na określenie koloru kapelusza skrzata na głowie.
+Jeżeli na zdjęciu nie będzie skrzata lub wystąpi inny błąd zwróć tylko i wyłącznie odpowiedź: "ERROR".`;
+
 const gnome = async function () {
   await postGetTaskToken('gnome');
   const { msg, url } = await getTaskDetails<GnomeTaskResponse>();
 
-  const { message } = await checkAiImage(msg, url);
+  const { message } = await checkAiImage(prompt, url);
   await postTaskAnswer({ answer: message.content });
 };
 
